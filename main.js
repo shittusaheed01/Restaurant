7cc096e1796efc397c571ce2117e50b746d273e3
Saheed <shittusaheed01@gmail.com>
2021-01-03 12:35:32 +0100

    added tab icon

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ menuBtn.addEventListener("click", () => {
 
 const checkBox = document.getElementsByClassName("checks")
 const quantity = document.getElementsByClassName("quantities")
-
+// console.log(checkBox[3]);
 // console.log(quantity[1].value);
 const price = document.getElementsByClassName("price")
   let rPrice = []
   for (let i = 0; i < price.length; i++) {
     rPrice.push(price[i].innerText.replace("#", ""));
   }
-  console.log(rPrice);
+  // console.log(rPrice);
 
 const total = document.getElementById("total")
 const purchase = document.getElementById("purchase");
@@ -29,20 +29,22 @@ for (var i = 0; i < checkBox.length; i++){
   checkB.addEventListener("change", checkthis)
 }
 function checkthis(){
-  
   let sum = 0;
   for(let j =0; j< checkBox.length; j++){
   if (checkBox[j].checked) {
-    console;
     sum += quantity[j].value * rPrice[j];
+    var input = quantity[j];
+    input.addEventListener("change", quantityChanged);
+    updateCartTotal();
   } else {
     // quantity[j].value = 0;
     sum += 0;
     // console.log(rPrice);
   }
-  console.log(sum)
+  // console.log(sum)
   total.innerText = "#" + sum;
 }
+
 }
 
 
@@ -50,13 +52,26 @@ function checkthis(){
 
 
   // input
-  for (var i = 0; i < quantity.length; i++) {
-    var input = quantity[i];
-    input.addEventListener("change", quantityChanged);
-  }
+  // for (var i = 0; i < quantity.length; i++) {
+  //   var input = quantity[i];
+  //   input.addEventListener("change", quantityChanged);
+  // }
 function quantityChanged(event) {
   var input = event.target;
   if (isNaN(input.value) || input.value <= 0) {
     input.value = 1;
   }
+  updateCartTotal();
+}
+function updateCartTotal(){
+  const orderDiv = document.getElementsByClassName("order-options")
+  let total = 0;
+  for(let i =0; i<orderDiv.length; i++){
+    let input = document.getElementsByClassName("quantities")[i].value;
+    let price = document.getElementsByClassName("price")[i].innerText.replace("#", "");
+    console.log(price);
+    total += input * price
+  }
+  document.getElementById("total").innerHTML ="#" + total;
+  // console.log(total)
 }
